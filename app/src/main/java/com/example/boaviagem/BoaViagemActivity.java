c90a1d246cc59ac4cb9c403e5d7564e3ac8472ab
ZNitr0 <levidonascimento2410@gmail.com>
2025-12-02 21:07:05 -0300

    colocando checbox de manter_login

--- app/src/main/java/com/example/boaviagem/BoaViagemActivity.java
+++ app/src/main/java/com/example/boaviagem/BoaViagemActivity.java
@@ -2,17 +2,20 @@ package com.example.boaviagem;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class BoaViagemActivity extends Activity {
 
-
-
+    private static final String MANTER_CONECTADO = "manter_conectado";
     private EditText usuario;
     private EditText senha;
+
+    private CheckBox manterConectado;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,8 +23,21 @@ public class BoaViagemActivity extends Activity {
         setContentView(R.layout.login);
 
         getActionBar().setTitle("Login");
+
         usuario = (EditText) findViewById(R.id.usuario);
         senha = (EditText) findViewById(R.id.senha);
+        manterConectado = (CheckBox) findViewById(R.id.manterConectado);
+
+        SharedPreferences preferences =
+                getPreferences(MODE_PRIVATE);
+        boolean conectado =
+                preferences.getBoolean(MANTER_CONECTADO, false);
+
+        if(conectado){
+            startActivity(
+                    new Intent(this, DashboardActivity.class)
+            );
+        }
     }
 
     public void entrarOnclick(View v) {
@@ -32,8 +48,14 @@ public class BoaViagemActivity extends Activity {
         if (usuarioInformado.equals("leitor") &&
                 senhaInformada.equals("123")) {
 
-            Intent intent = new Intent(this, DashboardActivity.class);
-            startActivity(intent);
+            SharedPreferences preferences =
+                    getPreferences(MODE_PRIVATE);
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean(MANTER_CONECTADO,manterConectado.isChecked());
+            editor.commit();
+
+            startActivity(new Intent(this, DashboardActivity.class));
 
         } else {
             String mensagemErro = getString(R.string.erro_autenticacao);
